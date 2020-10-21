14a26a598f3979b771025b58c0cb17edce0a65ec
BitYoungjae <bityoungjae@gmail.com>
2020-10-21 10:45:27 +0900

    fix: 구글 인증이 안되는 문제 수정

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, session } from 'electron';
 import { persistWindowState, getWindowState } from './utils/persistWindowState';
 import { createMainWindow } from './window';
 import { initWindowIPC } from './ipc/initWindowIPC';
@@ -50,6 +50,16 @@ const initMainWindow = () => {
 };
 
 app.on('ready', () => {
+  /* 구글 인증 페이지에서만 요청 헤더 중 userAgent를 크롬으로 변경해 전송한다.
+   * 구글 인증이 안되는 문제에 대한 미봉책. */
+  session.defaultSession.webRequest.onBeforeSendHeaders(
+    { urls: ['https://accounts.google.com/*'] },
+    (details, callback) => {
+      details.requestHeaders['User-Agent'] = 'Chrome';
+      callback({ cancel: false, requestHeaders: details.requestHeaders });
+    }
+  );
+
   initMainWindow();
 });
 
