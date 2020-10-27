b5935703fcdd60aa49218d6a50e1555a5196c345
BitYoungjae <bityoungjae@gmail.com>
2020-10-27 10:45:33 +0900

    build: watch 관련 빌드 스크립트 변경

--- watch.sh
+++ watch.sh
@@ -6,12 +6,11 @@ if [ -z "$(byobu list-sessions | grep -w ${SESSION_NAME})" ]; then
     byobu-tmux new-session -d -t "${SESSION_NAME}" 
 
     byobu-tmux split-window -t "${SESSION_NAME}:0.0" -h \; split-window -t "${SESSION_NAME}:0.1" -v
-    byobu-tmux send-keys -t "${SESSION_NAME}:0.0" 'npx tsc -w -p ./tsconfig.json' 'C-m'
+    byobu-tmux send-keys -t "${SESSION_NAME}:0.0" 'npx tsc -w' 'C-m'
     byobu-tmux send-keys -t "${SESSION_NAME}:0.1" 'npx webpack -w' 'C-m'
     echo "Start nodemon..."
     sleep 10
-    byobu-tmux send-keys -t "${SESSION_NAME}:0.2" "npx nodemon -w out --exec 'electron .'" 'C-m'
-
+    byobu-tmux send-keys -t "${SESSION_NAME}:0.2" "npx nodemon -w out --exec 'cross-env NODE_ENV=development electron .'" 'C-m'
     byobu-tmux select-pane -t "${SESSION_NAME}:0"
 fi
 
