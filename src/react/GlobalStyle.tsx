541fdf9dcbac6eca705daf180dfde4edd27538df
BitYoungjae <bityoungjae@gmail.com>
2020-11-02 19:02:20 +0900

    noto sans 폰트 적용

--- src/react/GlobalStyle.tsx
+++ src/react/GlobalStyle.tsx
@@ -1,12 +1,22 @@
 import { createGlobalStyle } from 'styled-components';
 import { normalize } from 'styled-normalize';
 import { TITLEBAR_HEIGHT } from './constants';
+import NotoSans from './noto-sans-kr.woff2';
 
 const GlobalStyle = createGlobalStyle`
+@font-face {
+  font-family: 'Noto Sans';
+  src: url(${NotoSans});
+  font-style: normal;
+  font-weight: 400;
+}
+
 ${normalize};
+
 html {
   height: 100%;
   text-size-adjust: 100%;
+  font-family: 'Noto Sans';
   font-size: 16px;
 }
 body {
