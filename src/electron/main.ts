ed02f59418e9fdf5097ac05fba23cfb761345e94
BitYoungjae <bityoungjae@gmail.com>
2023-02-14 16:38:03 +0900

    Upgrade to 1.0.7 (#68)
    * userAgent 수정 변경
    
    * 1.0.6
    
    * 1.0.7
    
    * 문서 수정

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -15,14 +15,18 @@ logger.transports.file.level = 'info';
 logger.log('App starting..');
 
 app.on('ready', () => {
-  /* 구글 인증 페이지에서만 요청 헤더 중 userAgent를 크롬으로 변경해 전송한다.
+  /* 구글 인증 페이지에서만 요청 헤더 중 userAgent를 변경해 전송한다.
    * 구글 인증이 안되는 문제에 대한 미봉책.
    * 해결책 링크 : https://developers.google.com/identity/protocols/oauth2/javascript-implicit-flow
    * */
   session.defaultSession.webRequest.onBeforeSendHeaders(
     { urls: ['https://accounts.google.com/*'] },
     (details, callback) => {
-      details.requestHeaders['User-Agent'] = 'Chrome';
+      const userAgentBefore = details.requestHeaders['User-Agent'];
+      details.requestHeaders['User-Agent'] = userAgentBefore.replace(
+        /Electron\/.*/,
+        ''
+      );
       callback({ cancel: false, requestHeaders: details.requestHeaders });
     }
   );
