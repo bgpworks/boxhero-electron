d54c68925dba92fecc487d6c97b30f667101b797
BitYoungjae <bityoungjae@gmail.com>
2023-02-14 12:03:51 +0900

    문서 수정

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -15,7 +15,7 @@ logger.transports.file.level = 'info';
 logger.log('App starting..');
 
 app.on('ready', () => {
-  /* 구글 인증 페이지에서만 요청 헤더 중 userAgent를 크롬으로 변경해 전송한다.
+  /* 구글 인증 페이지에서만 요청 헤더 중 userAgent를 변경해 전송한다.
    * 구글 인증이 안되는 문제에 대한 미봉책.
    * 해결책 링크 : https://developers.google.com/identity/protocols/oauth2/javascript-implicit-flow
    * */
