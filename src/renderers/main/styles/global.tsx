eb64104433d9e66b59f858c3bd83ebf3dc5eea0b
BitYoungjae <bityoungjae@gmail.com>
2023-10-19 17:46:32 +0900

    pretendard 적용

--- src/renderers/main/styles/global.tsx
+++ src/renderers/main/styles/global.tsx
@@ -1,19 +1,11 @@
 import { createGlobalStyle } from "styled-components";
 import { TITLEBAR_HEIGHT } from "../constants";
-import NotoSans from "./noto-sans-kr.woff2";
 
 const GlobalStyle = createGlobalStyle`
-@font-face {
-  font-family: 'Noto Sans';
-  src: url(${NotoSans});
-  font-style: normal;
-  font-weight: 400;
-}
-
 html {
   height: 100%;
   text-size-adjust: 100%;
-  font-family: 'Noto Sans';
+  font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
   font-size: 16px;
 }
 
