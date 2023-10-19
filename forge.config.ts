e8f5a294d4e963a45d48a17482a18bad6df623f6
BitYoungjae <bityoungjae@gmail.com>
2023-10-19 19:06:21 +0900

    간단한 빌드 셋업

--- forge.config.ts
+++ forge.config.ts
@@ -1,19 +1,20 @@
 import type { ForgeConfig } from "@electron-forge/shared-types";
 import { MakerSquirrel } from "@electron-forge/maker-squirrel";
-import { MakerZIP } from "@electron-forge/maker-zip";
-import { MakerDeb } from "@electron-forge/maker-deb";
-import { MakerRpm } from "@electron-forge/maker-rpm";
+import { MakerDMG } from "@electron-forge/maker-dmg";
 import { VitePlugin } from "@electron-forge/plugin-vite";
 
 const config: ForgeConfig = {
-  packagerConfig: {},
+  packagerConfig: {
+    name: "BoxHero",
+    icon: "./build/icon",
+    osxSign: {
+      optionsForFile: () => {
+        return { entitlements: "./build/entitlements.mac.plist" };
+      },
+    },
+  },
   rebuildConfig: {},
-  makers: [
-    new MakerSquirrel({}),
-    new MakerZIP({}, ["darwin"]),
-    new MakerRpm({}),
-    new MakerDeb({}),
-  ],
+  makers: [new MakerSquirrel({}), new MakerDMG({ name: "BoxHero" })],
   plugins: [
     new VitePlugin({
       // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
