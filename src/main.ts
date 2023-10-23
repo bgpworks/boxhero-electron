f24328e9c0db3da5b012f0aadcc3b93bcecce5cb
BitYoungjae <bityoungjae@gmail.com>
2023-10-23 18:31:07 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -10,63 +10,67 @@ import { initViewEvents, updateViewState } from "./utils/manageViewState";
 import { isMainWindow, openBoxHero } from "./window";
 import electronSquirrelStartup from "electron-squirrel-startup";
 
-// @ts-ignore
-// NOTE: https://github.com/mongodb-js/electron-squirrel-startup
-if (electronSquirrelStartup) return;
+function main() {
+  // unhandled error도 catch 한다.
+  logger.catchErrors();
+  logger.transports.file.level = "info";
 
-// unhandled error도 catch 한다.
-logger.catchErrors();
-logger.transports.file.level = "info";
+  logger.log("App starting..");
 
-logger.log("App starting..");
+  app.on("ready", async () => {
+    /* 구글 인증 페이지에서만 요청 헤더 중 userAgent를 변경해 전송한다.
+     * 구글 인증이 안되는 문제에 대한 미봉책.
+     * 해결책 링크 : https://developers.google.com/identity/protocols/oauth2/javascript-implicit-flow
+     * */
+    session.defaultSession.webRequest.onBeforeSendHeaders(
+      { urls: ["https://accounts.google.com/*"] },
+      (details, callback) => {
+        const userAgentBefore = details.requestHeaders["User-Agent"];
+        details.requestHeaders["User-Agent"] = userAgentBefore.replace(
+          /Electron\/.*/,
+          ""
+        );
+        callback({ cancel: false, requestHeaders: details.requestHeaders });
+      }
+    );
 
-app.on("ready", async () => {
-  /* 구글 인증 페이지에서만 요청 헤더 중 userAgent를 변경해 전송한다.
-   * 구글 인증이 안되는 문제에 대한 미봉책.
-   * 해결책 링크 : https://developers.google.com/identity/protocols/oauth2/javascript-implicit-flow
-   * */
-  session.defaultSession.webRequest.onBeforeSendHeaders(
-    { urls: ["https://accounts.google.com/*"] },
-    (details, callback) => {
-      const userAgentBefore = details.requestHeaders["User-Agent"];
-      details.requestHeaders["User-Agent"] = userAgentBefore.replace(
-        /Electron\/.*/,
-        ""
-      );
-      callback({ cancel: false, requestHeaders: details.requestHeaders });
-    }
-  );
+    await initLocale();
+    initWindowIPC();
+    initViewIPC();
 
-  await initLocale();
-  initWindowIPC();
-  initViewIPC();
+    openBoxHero();
+  });
 
-  openBoxHero();
-});
+  app.on("browser-window-created", (_, newWindow) => {
+    newWindow.webContents.once("did-finish-load", () => {
+      if (isMainWindow(newWindow)) {
+        updateViewState(newWindow);
+        initViewEvents();
+      }
+    });
+  });
 
-app.on("browser-window-created", (_, newWindow) => {
-  newWindow.webContents.once("did-finish-load", () => {
-    if (isMainWindow(newWindow)) {
-      updateViewState(newWindow);
-      initViewEvents();
+  app.on("browser-window-focus", (_, focusedWindow) => {
+    if (isMainWindow(focusedWindow)) {
+      updateViewState(focusedWindow);
     }
   });
-});
 
-app.on("browser-window-focus", (_, focusedWindow) => {
-  if (isMainWindow(focusedWindow)) {
-    updateViewState(focusedWindow);
-  }
-});
+  app.on("window-all-closed", () => {
+    if (!isMac) app.quit();
 
-app.on("window-all-closed", () => {
-  if (!isMac) app.quit();
+    logger.debug("all window closed");
+  });
 
-  logger.debug("all window closed");
-});
+  app.on("activate", (_, hasVisibleWindows) => {
+    if (!hasVisibleWindows) openBoxHero();
+  });
 
-app.on("activate", (_, hasVisibleWindows) => {
-  if (!hasVisibleWindows) openBoxHero();
-});
+  updateElectronApp({ logger, updateInterval: "30 minutes" });
+}
 
-updateElectronApp({ logger, updateInterval: "30 minutes" });
+// @ts-ignore
+// NOTE: https://github.com/mongodb-js/electron-squirrel-startup
+if (!electronSquirrelStartup) {
+  main();
+}
